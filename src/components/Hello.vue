5accac04dc8e00a3110545cf456da6cf05d6df26
chengeyangyang <390518881@qq.com>
2018-05-28 13:28:56 +0800

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+      <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: '第一次的跳转'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello{
+  background-color: azure;
+  margin: auto;
+}
+
+</style>
