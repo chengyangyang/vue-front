03964ef17c286c07c5d78382c0b357ca369ce7fb
lixiaoxiao <12324>
2018-06-08 16:04:44 +0800

    shiyan

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import vuex from 'vuex'
-import dialog_store from '../components/dialog_store.js'
+import dialog_store from './dialog_store.js'
 Vue.use(vuex)// 引入某个store对象
 export default new vuex.Store({
   modules: {
