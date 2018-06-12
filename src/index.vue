2af7570bd19a56c90704e322345bbd7005a0f443
lixiaoxiao <12324>
2018-06-12 10:39:37 +0800

    shiyan

---
+++ src/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="index">
+    2222222222222222
+    <header></header>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+import header from "./header.vue"
+import footer from "./footer.vue"
+export default {
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    header,
+    footer
+  }
+}
+</script>
+
+<style>
+#index {
+}
+</style>
