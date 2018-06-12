240aa82dfd164d8488805f76efdb158ea4ec1249
lixiaoxiao <12324>
2018-06-12 10:01:54 +0800

    shiyan

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/common.css'
+// import '../node_modules/vux/src/styles/reset.less';
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
