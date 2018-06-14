84be1a4e91c404a3bbc6bd651e3a44cea911ba5c
lixiaoxiao <12324>
2018-06-14 10:49:41 +0800

    Merge branch 'master' of https://github.com/chengyangyang/vue-front
    # Conflicts:
    #	src/main.js

---
+++ src/components/pageNation.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="pageNation">
+      <div class="block" style="border: 1px solid  #E6E6E6;background: rgb(255,255,255);padding: 15px;text-align: center">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400">
+        </el-pagination>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      currentPage4: 4
+    }
+  },
+  methods:{
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+#pageNation{
+  margin-top: 10px;
+}
+
+</style>
